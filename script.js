da5ac97774a9b6affcf98541e5e8c0e0a72d4bd4
Estesq <rohan.pawar@helixstack.in>
2022-09-30 20:47:43 +0530

    Consent window added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// cookies
+const cookie_box = document.getElementById("cookie_box");
+
+window.addEventListener("load", () => {
+  if(!navigator.cookieEnabled){
+    cookie_box.classList.add("show");
+  }
+  else{
+    cookie_box.classList.add("hide");
+    cookie_box.classList.remove("show");
+  }
+})
+
 //OPEN POPUP
 document.querySelector(".circle").addEventListener("click", () => {
   document.querySelector(".circle").classList.remove("deactive");
@@ -56,3 +69,31 @@ const teamMember = (memberIndex) => {
   window.document.querySelector("#team-member-position").textContent = member[memberIndex].Position;
   window.document.querySelector("#team-member-scope").textContent = member[memberIndex].Scope;
 }
+
+
+
+
+// cookies
+const acceptCookie = () => {
+  cookie_box.classList.remove("show")
+  cookie_box.classList.add("hide");
+
+  var cookieName = "ITFlow";
+  var cookieValue = "ITFlowCookies";
+  var cookieExpireDays = 30;
+  createCookie(cookieName, cookieValue, cookieExpireDays);
+}
+
+let createCookie = (cookieName, cookieValue, cookieExpireDays) => {
+  let currentDate = new Date();
+  currentDate.setTime(currentDate.getTime() + (cookieExpireDays*24*60*60*1000));
+
+  let expires = "expires=" + currentDate.toGMTString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+  if(document.cookie){
+    document.getElementById("cookie_box").style.display = "none";
+  }
+  else{
+    alert("Unable to set cookie. Please allow all cookies site from cookie setting of your browser");
+  }
+ }
